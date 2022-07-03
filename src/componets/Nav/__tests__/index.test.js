e1d7cf6d39d9bec7883759121c3adf0079bbc82c
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-07-03 11:27:08 -0400

    gallery upload

--- src/componets/Nav/__tests__/index.test.js
+++ src/componets/Nav/__tests__/index.test.js
@@ -1,40 +1,37 @@
-import React from "react";
-import {render, cleanup} from '@testing-library/react';
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
-
 afterEach(cleanup);
 
-describe('Nav componets', ()=>{
-    // baseline test
-    it("render",()=>{
-        render(<Nav/>);
-    })
+describe('Nav component', () => {
+  it('renders', () => {
+    render(<Nav />);
+  });
 
-    // snapshot test
-    it('matches snapshot',()=>{
-        const {asFragment}= render(<Nav/>);
-        expect(asFragment()).toMatchSnapshot();
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
 
-    })
-});
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
 
-describe("emoji is visible",()=>{
-    it('inserts emoji into the h2',()=>{
-        // Arrange
-        const {getByLabelText}=render(<Nav/>);
-        // Assert
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-    
-});
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
 
-describe('links are visible',()=>{
-    it('inserts text into the links',()=>{
-     const {getByTestId}=render(<Nav/>);
-     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-     expect(getByTestId('about')).toHaveTextContent('About me');
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})
 
-    });
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
 })
